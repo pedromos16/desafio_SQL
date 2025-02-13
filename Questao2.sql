e979b43707084a50b948d4869ecc85f49497d39a
pedromos16 <pedro.marcio.os7@gmail.com>
2025-02-13 01:14:36 -0300

    inserts

--- Questao2.sql
+++ Questao2.sql
@@ -20,10 +20,36 @@ VALUES (3, 2, 'Escolha das regioes atendidas', NULL, 'Nao iniciado');
 
 
 INSERT INTO Usuarios (Id, Nome, Email, Telefone, Cargo) 
-VALUES (1, 'Pedro', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Trainee');
+VALUES (1, 'Pedro', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Desenvolvedor');
 
 INSERT INTO Usuarios (Id, Nome, Email, Telefone, Cargo) 
-VALUES (2, 'Marcelo', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Senior');
+VALUES (2, 'Marcelo', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Desenvolvedor');
 
 INSERT INTO Usuarios (Id, Nome, Email, Telefone, Cargo) 
-VALUES (3, 'Ivan', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Senior');
+VALUES (3, 'Ivan', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Desenvolvedor');
+
+INSERT INTO Usuarios (Id, Nome, Email, Telefone, Cargo) 
+VALUES (4, 'Bruna', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Tech Recruiter');
+
+INSERT INTO Usuarios (Id, Nome, Email, Telefone, Cargo) 
+VALUES (5, 'Carlos', 'kathlyn9847@uorak.com', '(83) 3038-5432', 'Analista Financeiro');
+
+
+INSERT INTO Equipes (Id, Nome, Setor, Responsavel, Descricao) 
+VALUES (1, 'Azul', 'Desenvolvimento', 'Marcelo', 'Equipe de desenvolvimento de software');
+
+INSERT INTO Equipes (Id, Nome, Setor, Responsavel, Descricao) 
+VALUES (2, 'Verde', 'Recursos Humanos', 'Bruna', 'Atracao de talentos e gestao interna');
+
+INSERT INTO Equipes (Id, Nome, Setor, Responsavel, Descricao) 
+VALUES (3, 'Laranja', 'Financeiro', 'Carlos', 'Gestao finaceira da empresa');
+
+
+INSERT INTO Usuarios_Equipes (IdUsuario, IdEquipe, DataEntrada, Funcao, Ativo) 
+VALUES (1, 1, '2025-02-21', 'Desenvolvedor Trainee', 'Sim');
+
+INSERT INTO Usuarios_Equipes (IdUsuario, IdEquipe, DataEntrada, Funcao, Ativo) 
+VALUES (4, 2, '2024-04-15', 'Tech Recruiter', 'Sim');
+
+INSERT INTO Usuarios_Equipes (IdUsuario, IdEquipe, DataEntrada, Funcao, Ativo) 
+VALUES (5, 3, '2023-07-03', 'Analista financeiro', 'Nao');
